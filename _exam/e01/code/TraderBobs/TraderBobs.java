f6a7781d0a7e01bca9c45b0ecadcd33a5a747acb
Phill Conrad <pconrad@cs.ucsb.edu>
2020-02-17 23:28:45 -0800

    pc - first draft of TR version

--- _exam/e01/code/TraderBobs/TraderBobs.java
+++ _exam/e01/code/TraderBobs/TraderBobs.java
@@ -1,95 +1,99 @@
 public class TraderBobs {
 
-  public static void TB01 () {
-    Product p1 = new Beverage(99,"Coke",150,12.0);
-    System.out.println("p1: " + p1.getName());
+  public static void TB20 () {
+      //      Product p20 = new Product(299, "Fuzzy Dice");
+      //System.out.println(p20.getPrice());
   }
-  public static void TB02 () {
-    Edible e2 = new Food(249,"Kind Bar",200,1.4);
-    //System.out.println("e2: " + e2.getName());
+    
+  public static void TB21 () {
+    Product p21 = new Food(249,"Kind Bar",200,1.4);
+    System.out.println(p21.getName());
   }
-
-  public static void TB03 () {
-    Edible e03 = ()->42 ;
-    System.out.println("e03: " + e03.getCalories());
-  }
-
-  public static void TB04 () {
-    Food f4 = new Food(199,"Gummi Bears",520,5);
-    System.out.println("f4: " + f4.getPrice());
+    
+  public static void TB22 () {
+      //Edible e22 = new Product(299, "Fuzzy Dice");
+      //System.out.println(e22.getCalories());
   }
 
-  public static void TB05 () {
-    FreeCandy f5 = new FreeCandy(50);
-    System.out.println("f5: " + f5.getCalories());
+  public static void TB23 () {
+    Edible e23 = ()->99 ;
+    System.out.println(e23.getCalories());
   }
 
-  public static void TB06 () {
-    Food f6 = new Food(99,"Peanuts",100,0.63);
-    System.out.println("f6: " + f6.getName());
-  }
-
-  public static void TB07 () {
-    Beverage b7 = new Beverage(89,"Diet Coke",0,12.0);
-    System.out.println("b7: " + b7.getCalories());
-  }
-
-  public static void TB08 () {
-    Product p8 = new Beverage(199,"Milk",120,6.75);
-    //System.out.println("p8: " + p8.getCalories());
-  }
-
-  public static void TB09 () {
-    Edible e9 = new FreeCandy(42);
-    System.out.println("e9: " + e9.getCalories());
-  }
-
-
-  public static void TB10 () {
-    Edible e10 = new Edible() {
+  public static void TB24 () {
+    Edible e24 = new Edible() {
       public int getCalories() {
-        return 100;
+        return 75;
       }
     };
-    System.out.println("e10: " + e10.getCalories());
+    System.out.println(e24.getCalories());
+  }
+
+  public static void TB25 () {
+    Food f25 = new Food(199,"Gummi Bears",520,5);
+    System.out.println(f25.getWeight());
+  }
+
+  public static void TB26 () {
+    FreeCandy f26 = new FreeCandy(50);
+    //System.out.println(f26.getName());
   }
 
 
-  public static void TB11 () {
-      //Product p11 = new Product(299,"Ziploc Bags");
-      //System.out.println("p11: " + p11.getPrice());
+  public static void TB27 () {
+    Product p27 = new Beverage(199,"Milk",120,6.75);
+    System.out.println(p27.getName());
   }
 
-  public static void TB12 () {
-    Food f12 = new Food(99,"Peanuts",100,0.63);
-    //System.out.println("f12: " + f12.getFluidOunces());
+  public static void TB28 () {
+    Food f28 = new Food(99,"Peanuts",100,0.63);
+    System.out.println(f28.getPrice());
   }
 
+  public static void TB29 () {
+    Edible e29 = new Beverage(89,"Diet Coke",0,12.0);
+    System.out.println(e29.getCalories());
+  }
+
+  public static void TB30 () {
+      // Product p30 = new FreeCandy(42);
+      //System.out.println(p30.getName());
+  }
+
+  public static void TB31 () {
+    Beverage b31 = new Beverage(89,"Diet Coke",0,12.0);
+    System.out.println(b31.getFluidOunces());
+  }
+
+  public static void TB32 () {
+    Product b32 = new Beverage(89,"Diet Coke",0,12.0);
+    System.out.println(b32.getPrice());
+  }
 
   public static void main (String [] args) {
-    System.out.println("TB01");
-    TB01();
-    System.out.println("TB02");
-    TB02();
-    System.out.println("TB03");
-    TB03();
-    System.out.println("TB04");
-    TB04();
-    System.out.println("TB05");
-    TB05();
-    System.out.println("TB06");
-    TB06();
-    System.out.println("TB07");
-    TB07();
-    System.out.println("TB08");
-    TB08();
-    System.out.println("TB09");
-    TB09();
-    System.out.println("TB10");
-    TB10();
-    System.out.println("TB11");
-    TB11();
-    System.out.println("TB12");
-    TB12();
+    System.out.println("TB21");
+    TB21();
+    System.out.println("TB22");
+    TB22();
+    System.out.println("TB23");
+    TB23();
+    System.out.println("TB24");
+    TB24();
+    System.out.println("TB25");
+    TB25();
+    System.out.println("TB26");
+    TB26();
+    System.out.println("TB27");
+    TB27();
+    System.out.println("TB28");
+    TB28();
+    System.out.println("TB29");
+    TB29();
+    System.out.println("TB30");
+    TB30();
+    System.out.println("TB31");
+    TB31();
+    System.out.println("TB32");
+    TB32();
   }
 }
---
+++ _exam/e01/cs56-w20-e01/handout_b.md
@@ -0,0 +1,157 @@
+---
+layout: examHandoutNoName
+num: e01
+ready: true
+desc: "Handout B"
+qtr: F19
+---
+
+<style>
+div.page1 { font-size: 100%;
+ line-height: 97%;
+}
+
+div.page1 h2 {
+ font-size: 105%;
+ font-family: Arial, sans-serif;
+ padding-top: 0px; padding-bottom: 0px;
+ margin-top: 0px; margin-bottom: 0px;
+}
+
+div.page2 {
+ font-size: 100%;
+ line-height: 103%;
+}
+
+div.page2 h2 {
+ font-size: 105%;
+ font-family: Arial, sans-serif;
+ padding-top: 2px; padding-bottom: 2px;
+ margin-top: 2px; margin-bottom: 2px;
+}
+
+
+td  {
+  padding-left:5px; padding-right: 5px;
+}
+
+hr { display:none;}
+
+</style>
+<div class="page1">
+
+
+## FreeCandy.java
+
+{% highlight java linenos %}
+public class FreeCandy implements Edible {
+
+    private int calories;
+    
+    public FreeCandy(int calories) { 
+	this.calories = calories;
+    }
+
+    public int getCalories() {return this.calories;}
+}
+{% endhighlight %}
+
+
+
+## Product.java
+
+
+{% highlight java linenos %}
+public abstract class Product {
+    String name;
+    int price;
+    
+    public int getPrice() { return price; } 
+    public String getName() {return name;}
+
+    public Product(int price, String name) {
+	this.price = price;
+	this.name = name;
+    }
+}
+{% endhighlight %}
+
+
+</div>
+
+<div class="page2 page-break-before">
+
+<h2 markdown="1">Handout B, p. 2</h2>
+
+
+<style>
+ div.dog { font-size: 110%;
+    line-height: 110%;
+        margin: 0.5em;
+	    padding: 0.5em;
+	         width: 65%;
+
+ }
+
+div.dog * td  {
+  padding-left:5px; padding-right: 5px;
+    }
+     div.fill-in-blanks-smaller {
+        width: 30%;
+	   float: right;
+	    }
+
+ .fill-in-blanks-smaller table {
+
+  }
+   .fill-in-blanks-smaller table * td {
+      margin: 0.5em 0.5em 0.5em 0.5em;
+         padding: 0.5em 0.5em 0.5em 0.5em;
+	    font-size: 150%;
+	       line-height: 150%;
+	        }
+
+</style>
+
+
+<div class="dog" markdown="1">
+{% highlight java linenos %}
+public class Dog {
+
+    private static Dog bestInShow = null;
+    private String name;
+    
+    public static void setBestInShow(Dog b) {
+	bestInShow = b;
+    }
+    
+    public static Dog getBestInShow() {
+	return bestInShow;
+    }
+    
+    public Dog(String name) { this.name = name;}
+    
+    public static void main(String [] args) {
+	
+	Dog d1 = new Dog("Fido");
+	Dog d2 = new Dog("Ginger");
+	Dog d3 = new Dog("Harry");
+	Dog d4 = new Dog("Izzy");
+	Dog d5 = new Dog("Jack");
+	Dog d6 = d5;
+	
+	setBestInShow(d1);
+	d3 = d2;
+	d5 = d4;
+	d6 = null;
+	d5 = null;
+	d4 = null;
+	d3 = null;
+	d2 = null;
+	d1 = null;
+	setBestInShow(null);
+    }
+}
+{% endhighlight %}
+
+</div><!-- dog -->
